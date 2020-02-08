920eda361a79163f15ccc72ab728eda7fcdfadab
Pallav Agarwal <agpallav@gmail.com>
2020-02-08 01:48:07 -0500

    Completed Twitter Part 1

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -15,11 +15,21 @@ class LoginViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true { //Get from memory
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
         
         let myUrl = "https://api.twitter.com/oauth/request_token"
         
+        //Login Handling
         TwitterAPICaller.client?.login(url: myUrl, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn") //Store in memory
+            
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Could not log in!")
